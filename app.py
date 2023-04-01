1d89183f7effc879804ed88aeba8f613a3aac78b
Omurbek Dulatov <oma.dulatov@gmail.com>
2023-04-01 21:02:34 +0600

    Feature

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import locale
 from datetime import datetime, timedelta
+import smtplib
 
 from flask import Flask, render_template
 
-from models import db, User, Position
+from models import db, User, Position, Skill, Resume, Summary, Education
 from utils import init_app
 
 app = Flask(__name__)
@@ -16,6 +17,10 @@ locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
 def index():
     user = db.session.query(User).first()
     position = db.session.query(Position).filter_by(user_id=user.id).first()
+    skills = db.session.query(Skill).filter_by(position_id=position.id).all()
+    resume = db.session.query(Resume).filter_by(position_id=position.id).first()
+    summary = db.session.query(Summary).filter_by(resume_id=resume.id).first()
+    educations = db.session.query(Education).filter_by(resume_id=resume.id).all()
 
     user_info = {
         'first_name': user.first_name,
@@ -24,13 +29,44 @@ def index():
         'age': datetime.today().date().year - user.date_of_birth.year,
         'phone_number': user.phone_number,
         'address': user.address,
+        'email': user.email,
         'position': {
             'name': position.name,
             'description': position.description,
+            'freelance': position.freelance,
+            'skills': skills,
+            'resume': {
+                'description': resume.description,
+                'summary': summary,
+                'educations': educations,
+            }
         }
     }
 
     return render_template('index.html', user_info=user_info)
+
+
+@app.route('/send-mail')
+def send_mail():
+    smtp_server = "smtp.elasticemail.com"
+    port = 2525
+    username = "your_username"
+    password = "your_password"
+
+    from_email = "you@example.com"
+    to_email = "recipient@example.com"
+    subject = "Test Email"
+    body = "This is a test email sent via ElasticEmail and Python 3"
+
+    message = f"From: {from_email}\nTo: {to_email}\nSubject: {subject}\n\n{body}"
+    try:
+        smtpObj = smtplib.SMTP(smtp_server, port)
+        smtpObj.login(username, password)
+        smtpObj.sendmail(from_email, to_email, message)
+        print("Email sent successfully")
+    except Exception as e:
+        print("Error: unable to send email")
+        print(e)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Boolean
 
 db = SQLAlchemy()
 
@@ -12,6 +12,7 @@ class User(db.Model):
     phone_number = Column(String(20))
     address = Column(String(512))
     about = Column(String(512))
+    email = Column(String(128))
 
 
 class Position(db.Model):
@@ -19,4 +20,53 @@ class Position(db.Model):
     user_id = Column(Integer, ForeignKey(User.id))
     name = Column(String(128))
     description = Column(String(512))
+    freelance = Column(Boolean, default=False)
 
+
+class Skill(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    level = Column(Integer)
+    position_id = Column(Integer, ForeignKey(Position.id))
+
+
+class Resume(db.Model):
+    id = Column(Integer, primary_key=True)
+    description = Column(String(512))
+    position_id = Column(Integer, ForeignKey(Position.id))
+
+
+class Summary(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    intro = Column(String(512))
+    city = Column(String(128))
+    email = Column(String(128))
+    phone = Column(String(128))
+    resume_id = Column(Integer, ForeignKey(Resume.id))
+
+
+class Education(db.Model):
+    id = Column(Integer, primary_key=True)
+    resume_id = Column(Integer, ForeignKey(Resume.id))
+    name = Column(String(128))
+    major = Column(String(128))
+    description = Column(String(512))
+    date_begin = Column(Date)
+    date_end = Column(Date)
+    city = Column(String(128))
+
+
+class Experience(db.Model):
+    id = Column(Integer, primary_key=True)
+    position_name = Column(String(128))
+    is_present = Column(Boolean, default=False)
+    date_begin = Column(Date)
+    date_end = Column(Date)
+    location = Column(String(128))
+
+
+class ExperienceDescription(db.Model):
+    id = Column(Integer, primary_key=True)
+    experience_id = Column(Integer, ForeignKey(Experience.id))
+    description = Column(String(512))
